7a127a3652e7e435eab4d8e866ca61be526db23d
HristoGetov <260064@via.dk>
2018-08-26 21:04:51 +0200

    Exercice1 - Assignment 1.1-1.2

---
+++ Exercice/Exercice1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            /*  for(i =100; i < 203; i+=2)
+               {
+                   Console.WriteLine(i);
+               }
+              for(int j=202; j > 100; j -= 2)
+               {
+                   Console.WriteLine(j);
+               }
+               */
+            while (i < 202)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
